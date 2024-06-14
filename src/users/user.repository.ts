8be396add599b11d71daceef535d68a136b6639c
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-06-14 13:22:38 +0900

    パスワードのハッシュ化完了

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -2,29 +2,25 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-// import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     console.log(createUserDto);
-    const user = this.create(createUserDto);
+
+    //パスワードをハッシュ化する処理
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const user = this.create({
+      ...createUserDto,
+      password: hashPassword, //パスワードをハッシュ化
+    });
     console.log(user);
+
     await this.save(user);
     return user;
-    // //パスワードをハッシュ化する処理
-    // const salt = await bcrypt.genSalt();
-    // const hashPassword = await bcrypt.hash(password, salt);
-
-    // const user = this.create({
-    //   name,
-    //   email,
-    //   password: hashPassword, //パスワードをハッシュ化
-    // });
-
-    // await this.save(user);
-
-    // return user;
   }
 
   async findById(id: number): Promise<User | undefined> {
